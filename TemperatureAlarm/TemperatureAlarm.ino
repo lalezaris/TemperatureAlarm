e7533f715244cf3ca1efec592cadd768818a4b93
Sam Lalezari <sam.lalezari@gmail.com>
2017-02-07 11:06:01 -0500

    Cleaned up code

--- TemperatureAlarm/TemperatureAlarm.ino
+++ TemperatureAlarm/TemperatureAlarm.ino
@@ -35,7 +35,6 @@ float setTemp;
 int cursorSetPos;
 unsigned long lastTime = 0;
 float lastTemp = 0;
-unsigned long lastButtonPressTime = 0;
 int previousSetButtonState = HIGH;
 int previousNextButtonState = HIGH;
 
@@ -48,204 +47,31 @@ void setup() {
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
+  
+  // Print message to the LCD.
   lcd.print("Cur. Temp:     F");
   lcd.setCursor(0, 1);
   lcd.print("Alarm Set:     F");
   lcd.setCursor(10, 1);
   dtostrf(setTemp, 5, 1, setTempChars);
   lcd.write(setTempChars, 5);
+
+  // Setup the buttons
   pinMode(SET_DIGIT_PIN, INPUT_PULLUP);
   pinMode(NEXT_DIGIT_PIN, INPUT_PULLUP);
 
+  // Open serial connection
   Serial.begin(9600);
 }
 
 void loop() {
-  //  // Serial.println("Starting loop... setting no blink");
-  //  lcd.noBlink();
-  // Serial.print("WAITING");
-
   // Wait to refresh LCD after 5 seconds and a change in
   // temperature of .1 degree f
   while (waitToRefresh(5000) || tempIsSame(.2)) {
     setBlinkingCursor();
     checkForButtonInput();
   }
-  // Serial.println();
-  // Serial.println(millis());
   writeTempToLCD();
-  /*
-    //  setCursorToSetPos();
-    // Set the cursor at current temp and record temp data
-    //  lcd.setCursor(10, 0);
-    //  // Serial.print("Requesting temp data......");
-    //  sensors.requestTemperatures();
-    //  // Serial.println("... Done.");
-    //  curTemp = sensors.getTempFByIndex(0);
-    //  // Serial.print("Writing to lcd.. cur temp(f): ");
-    //  // Serial.println(curTemp);
-    //  // Serial.print("cur temp(c): ");
-    //  // Serial.println(sensors.getTempCByIndex(0));
-    //  dtostrf(curTemp, 5, 1, curTempChars);
-    //  // Serial.print("cur temp chars: ");
-    //  // Serial.println(curTempChars);
-    //  lcd.write(curTempChars, 5);
-    //  // Serial.println("Done");
-
-    if(currentSetDigit > 0 && currentSetDigit <= 4){
-    lcd.setCursor(cursorSetPos,1);
-    lcd.cursor();
-    //    lcd.blink();
-    } else {
-    lcd.noCursor();
-    }
-
-    while(digitalRead(NEXT_DIGIT_PIN) == HIGH ){
-
-    if(digitalRead(SET_DIGIT_PIN) == LOW){
-      if(currentSetDigit == 1){
-
-        int curDigit = 0;
-          char curChar = setTempChars[0];
-        if(isdigit(curChar)){
-          curDigit = setTempChars[0] - '0';
-        }
-
-
-          if(curDigit >= 9){
-            setTemp -= 900;
-          } else {
-            setTemp += 100;
-          }
-
-          cursorSetPos = 10;
-          lcd.setCursor(cursorSetPos,1);
-
-        } else if(currentSetDigit == 2){
-            int curDigit = 0;
-              char curChar = setTempChars[1];
-            if(isdigit(curChar)){
-              curDigit = setTempChars[1] - '0';
-            }
-
-
-              if(curDigit >= 9){
-                setTemp -= 90;
-              } else {
-                setTemp += 10;
-              }
-          cursorSetPos = 11;
-          lcd.setCursor(cursorSetPos,1);
-
-        } else if(currentSetDigit == 3){
-          int curDigit = 0;
-              char curChar = setTempChars[2];
-            if(isdigit(curChar)){
-              curDigit = setTempChars[2] - '0';
-            }
-
-
-              if(curDigit >= 9){
-                setTemp -= 9;
-              } else {
-                setTemp += 1;
-              }
-          cursorSetPos = 12;
-          lcd.setCursor(cursorSetPos,1);
-
-        } else if(currentSetDigit == 4){
-          // Decimal place
-        }else if(currentSetDigit == 5){
-          int curDigit = 0;
-              char curChar = setTempChars[4];
-            if(isdigit(curChar)){
-              curDigit = setTempChars[4] - '0';
-            }
-
-
-              if(curDigit >= 9){
-                setTemp -= .9;
-              } else {
-                setTemp += .1;
-              }
-          cursorSetPos = 14;
-          lcd.setCursor(cursorSetPos,1);
-
-        }
-
-    //        delay(500);
-
-    //    lcd.noBlink();
-    lcd.setCursor(10,1);
-    dtostrf(setTemp, 5, 1, setTempChars);
-    lcd.write(setTempChars, 5);
-
-
-    }
-    //    delay(1000);
-    //    lcd.noBlink();
-    lcd.setCursor(10, 0);
-    // Serial.print("Requesting temp data......");
-    sensors.requestTemperatures();
-    // Serial.println("... Done.");
-    curTemp = sensors.getTempFByIndex(0);
-    // Serial.println("Writing to lcd.. cur temp: ");
-    // Serial.println(curTemp);
-    dtostrf(curTemp, 5, 1, curTempChars);
-    // Serial.print("cur temp chars: ");
-    // Serial.println(curTempChars);
-    lcd.write(curTempChars, 5);
-    // Serial.println("Done");
-
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-
-    }
-
-    // Check for Next Digit Button input
-    if(currentSetDigit >= 5){
-      currentSetDigit = 0;
-    } else {
-      currentSetDigit += 1;
-    }
-
-
-    // Set the lcd cursor to the digit to be set & blink
-    if(currentSetDigit <= 0){
-    //    lcd.noBlink();
-    } else if(currentSetDigit == 1){
-    cursorSetPos = 10;
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-    } else if(currentSetDigit == 2){
-    cursorSetPos = 11;
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-    } else if(currentSetDigit == 3){
-    cursorSetPos = 12;
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-    } else if(currentSetDigit == 4){
-    cursorSetPos = 13;
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-    }else if(currentSetDigit == 5){
-    cursorSetPos = 14;
-    lcd.setCursor(cursorSetPos,1);
-    //    lcd.blink();
-    } else {
-    //    lcd.noBlink();
-    }
-
-    // Serial.print("Set Digit: ");
-    // Serial.print(currentSetDigit);
-    // Serial.print(" Cursor Pos: ");
-    // Serial.println(cursorSetPos);
-
-    // Wait a little bit getting temperature data again
-    //  delay(10);
-  */
 }
 
 /* Function: waitToRefresh
@@ -264,6 +90,7 @@ boolean waitToRefresh(unsigned long waitInMillis) {
     return true;
   }
 }
+
 /* Function: tempIsSame
     Returns a boolean indicating if the previous temp reading and
     the current temp reading are within the threashold provided.
@@ -275,37 +102,24 @@ boolean waitToRefresh(unsigned long waitInMillis) {
 boolean tempIsSame(float sensitivity) {
   sensors.requestTemperatures();
   curTemp = sensors.getTempFByIndex(0);
+  
   float tempDiff = abs(curTemp - lastTemp);
-  //  // Serial.print("lastTemp: ");
-  //  // Serial.print(lastTemp);
-  //  // Serial.print(" curTemp: ");
-  //  // Serial.print(curTemp);
-  //  // Serial.print(" diff: ");
-  //  // Serial.print(tempDiff);
+  
   if (tempDiff < sensitivity) {
-    //    // Serial.println(" true");
     return true;
   } else {
     lastTemp = curTemp;
-    //    // Serial.println(" false");
     return false;
   }
 }
 
 void writeTempToLCD() {
+  sensors.requestTemperatures();
   lcd.noBlink();
   lcd.setCursor(10, 0);
-  // Serial.print("Requesting temp data......");
-  sensors.requestTemperatures();
-  // Serial.println("... Done.");
   curTemp = sensors.getTempFByIndex(0);
-  // Serial.println("Writing to lcd.. cur temp: ");
-  // Serial.println(curTemp);
   dtostrf(curTemp, 5, 1, curTempChars);
-  // Serial.print("cur temp chars: ");
-  // Serial.println(curTempChars);
   lcd.write(curTempChars, 5);
-  // Serial.println("Done");
   lcd.setCursor(cursorSetPos, 1);
   lcd.blink();
 }
@@ -417,16 +231,6 @@ void nextDigitButtonAction() {
     cursorSetPos ++;
   } else {
     cursorSetPos = 9;
-  }
-}
-
-boolean notBounce() {
-  unsigned long diff = abs(millis() - lastButtonPressTime);
-  if (diff > 100) {
-    lastButtonPressTime = millis();
-    return true;
-  } else {
-    return false;
   }
 }
 
